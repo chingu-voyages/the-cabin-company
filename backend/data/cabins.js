75f230e12abc777d8bcd8330d8b43e074fb895c9
Claire Smith <cmoorman11@gmail.com>
2021-05-17 09:26:56 -0400

    Added seeder for database

---
+++ backend/data/cabins.js
@@ -0,0 +1,60 @@
+const cabins = [
+    {
+        name: "Snowy Bungalow",
+        pricePerNight: 150,
+        address: {
+            street: "123 Main St",
+            city: "Portland",
+            state: "Maine",
+            zipCode: 14019
+        },
+        beds: 3,
+        baths: 2,
+        image: "https://www.pexels.com/photo/brown-house-near-pine-trees-covered-with-snow-754268/",
+
+    },
+    {
+        name: "Mountain Getaway",
+        pricePerNight: 275,
+        address: {
+            street: "123 Chingu Road",
+            city: "Denver",
+            state: "Colorado",
+            zipCode: 80014
+        },
+        beds: 5,
+        baths: 3,
+        image: "https://www.pexels.com/photo/brown-2-storey-house-2294125/",
+
+    },
+    {
+        name: "Woodland Escape",
+        pricePerNight: 175,
+        address: {
+            street: "9876 Javascript Road",
+            city: "Nashville",
+            state: "Tennessee",
+            zipCode: 37011
+        },
+        beds: 3,
+        baths: 1,
+        image: "https://www.pexels.com/photo/autumn-autumn-leaves-beautiful-color-206648/",
+
+    },
+    {
+        name: "Lush Meadow",
+        pricePerNight: 200,
+        address: {
+            street: "321 React Lane",
+            city: "Milwaukee",
+            state: "Wisconsin",
+            zipCode: 53201
+        },
+        beds: 2,
+        baths: 2,
+        image: "https://www.pexels.com/photo/autumn-autumn-leaves-beautiful-color-206648/",
+
+    }
+];
+
+export default cabins;
