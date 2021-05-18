5cc5470e1e58fa2dd7da6c1066c2854a7fef296d
adelinel <adeline.latruwe@gmail.com>
2021-05-18 11:40:27 -0700

    remove unused code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,3 @@
-import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
@@ -7,7 +6,6 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <StrictMode>
-    <ColorModeScript />
     <App />
   </StrictMode>,
   document.getElementById('root')
