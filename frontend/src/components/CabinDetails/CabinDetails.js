112705b7b271e218f5f84f6df04a44eabf15e929
adelinel <adeline.latruwe@gmail.com>
2021-05-25 13:47:40 -0700

    updated CabinDetails content

--- frontend/src/components/CabinDetails/CabinDetails.js
+++ frontend/src/components/CabinDetails/CabinDetails.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-export default function LandingPage() {
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+const CabinDetails = props => {
+  const { name, pricePerNight, address, beds, baths, image } =
+    props.location.state;
+  const classes = useStyles();
   return (
     <div>
-      <p>Hi!</p>
+      <img src={image}></img>
+
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            {address.city}, {address.state}
+          </Typography>
+          <Typography variant="body2" component="p">
+            {beds} beds
+            <br />
+            {baths} baths
+          </Typography>
+        </CardContent>
+      </Card>
+
+      {/* Calendar goes here */}
+      {/* Pricing goes here */}
+
+      <Button>Book</Button>
     </div>
   );
-}
+};
+export default CabinDetails;
