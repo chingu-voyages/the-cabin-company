5fbaeb6c880e664ed7742de6a886e6c589904f88
carlosabreuh <carlos@abreuh.com>
2021-05-26 17:49:05 -0400

    Landing page calendar-- still need to resize/contain

---
+++ frontend/src/components/DateRangePicker/DatePickerCalendar.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { format } from 'date-fns'
+import { enGB } from 'date-fns/locale'
+import { DateRangePickerCalendar, START_DATE } from 'react-nice-dates'
+import 'react-nice-dates/build/style.css'
+import CardContent from '@material-ui/core/CardContent';
+export default function DateRangePickerCalendarExample() {
+  const [startDate, setStartDate] = useState()
+  const [endDate, setEndDate] = useState()
+  const [focus, setFocus] = useState(START_DATE)
+  const handleFocusChange = newFocus => {
+    setFocus(newFocus || START_DATE)
+  }
+  return (
+<CardContent>
+    <div>
+      
+      <DateRangePickerCalendar
+        startDate={startDate}
+        endDate={endDate}
+        focus={focus}
+        onStartDateChange={setStartDate}
+        onEndDateChange={setEndDate}
+        onFocusChange={handleFocusChange}
+        locale={enGB}
+      />
+    </div>
+    </CardContent>
+  )
+}
