ec861f22ad82bd647dfa07834f945ba74002c213
adelinel <adeline.latruwe@gmail.com>
2021-05-27 12:44:06 -0700

    refactored MultiLineGrid component

--- frontend/src/components/LandingPage/MultiLineGrid.js
+++ frontend/src/components/LandingPage/MultiLineGrid.js
@@ -2,16 +2,25 @@ import React from 'react';
 import section2Images from './section2Images';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
 import Typography from '@material-ui/core/Typography';
 import './LandingPage.css';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
-    overflow: 'hidden',
-
-    backgroundColor: theme.palette.background.paper,
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 400,
   },
 }));
 
@@ -19,18 +28,33 @@ export default function MultiLineGrid() {
   const classes = useStyles();
   return (
     <div>
-      <Typography style={{ margin: '1em 0 1em 0' }} variant="h4">
+      <Typography style={{ margin: '1em 0 1em 1em' }} variant="h4">
         Explore nearby
       </Typography>
       <div className={classes.root}>
-        <GridList cols={4}>
+        <GridList cols={4.5} style={{ justifyContent: 'space-evenly' }}>
           {section2Images.map(image => (
-            <GridListTile
-              key={image.img}
-              classes={{ tile: 'multigrid-images', root: 'multigrid-div' }}
+            <Card
+              className={classes.root}
+              style={{ margin: '1em 0 1em 0' }}
+              variant="outlined"
             >
-              <img src={image.img} />
-            </GridListTile>
+              <CardMedia
+                className={classes.cover}
+                image={image.img}
+                alt={image.alt}
+              />
+              <div className={classes.details}>
+                <CardContent className={classes.content}>
+                  <Typography component="h5" variant="h5">
+                    {image.title}
+                  </Typography>
+                  <Typography variant="subtitle1" color="textSecondary">
+                    {image.desc}
+                  </Typography>
+                </CardContent>
+              </div>
+            </Card>
           ))}
         </GridList>
       </div>
