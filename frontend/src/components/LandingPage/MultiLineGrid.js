5e9db8760a805d04a588c25d2b1c667ee5754330
adelinel <adeline.latruwe@gmail.com>
2021-05-27 10:25:34 -0700

    added MultiLineGrid styling

--- frontend/src/components/LandingPage/MultiLineGrid.js
+++ frontend/src/components/LandingPage/MultiLineGrid.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import section2Images from './section2Images';
+import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import './LandingPage.css';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    overflow: 'hidden',
+
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 export default function MultiLineGrid() {
+  const classes = useStyles();
   return (
-    <div>
-      <div>
-        <GridList cols={2.5}>
-          {section2Images.map(image => (
-            <GridListTile key={image.img}>
-              <img src={image.img} />
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
+    <div className={classes.root}>
+      <GridList cols={4}>
+        {section2Images.map(image => (
+          <GridListTile
+            key={image.img}
+            classes={{ tile: 'multigrid-images', root: 'multigrid-div' }}
+          >
+            <img src={image.img} />
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   );
 }
