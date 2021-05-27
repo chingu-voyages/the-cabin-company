310cc2bdd38103fe3bbb6f66e9f56a2305eba4a4
adelinel <adeline.latruwe@gmail.com>
2021-05-27 11:08:46 -0700

    added section title

--- frontend/src/components/LandingPage/MultiLineGrid.js
+++ frontend/src/components/LandingPage/MultiLineGrid.js
@@ -3,6 +3,7 @@ import section2Images from './section2Images';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import Typography from '@material-ui/core/Typography';
 import './LandingPage.css';
 
 const useStyles = makeStyles(theme => ({
@@ -17,17 +18,22 @@ const useStyles = makeStyles(theme => ({
 export default function MultiLineGrid() {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <GridList cols={4}>
-        {section2Images.map(image => (
-          <GridListTile
-            key={image.img}
-            classes={{ tile: 'multigrid-images', root: 'multigrid-div' }}
-          >
-            <img src={image.img} />
-          </GridListTile>
-        ))}
-      </GridList>
+    <div>
+      <Typography style={{ margin: '1em 0 1em 0' }} variant="h4">
+        Explore nearby
+      </Typography>
+      <div className={classes.root}>
+        <GridList cols={4}>
+          {section2Images.map(image => (
+            <GridListTile
+              key={image.img}
+              classes={{ tile: 'multigrid-images', root: 'multigrid-div' }}
+            >
+              <img src={image.img} />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
     </div>
   );
 }
