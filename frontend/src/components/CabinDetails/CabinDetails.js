f027d7f21f8ab562d4db50ed66f69d9b58fa409c
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-06-03 14:22:34 -0700

    added calendar and pricing details styling

--- frontend/src/components/CabinDetails/CabinDetails.js
+++ frontend/src/components/CabinDetails/CabinDetails.js
@@ -9,6 +9,7 @@ import axios from 'axios';
 import LoadingSpinner from '../LoadingSpinner';
 import Message from '../Message';
 import DateRangePickerCalendar from './DateRangePicker/DateRangePickerCalendar';
+import Pricing from './Pricing/Pricing';
 import './CabinDetails.css';
 
 const useStyles = makeStyles({
@@ -28,13 +29,12 @@ const useStyles = makeStyles({
   },
   paypal: {
     width: 200,
-    margin: 20
-  }
+    margin: 20,
+  },
 });
 
 const CabinDetails = props => {
-  const { name, address, beds, baths, image, _id } =
-    props.location.state;
+  const { name, address, beds, baths, image, _id } = props.location.state;
 
   const [sdkLoaded, setSdkLoaded] = useState(false);
   const [booking, setBooking] = useState(null);
@@ -46,7 +46,9 @@ const CabinDetails = props => {
   //Add paypal script to the body of the page
   useEffect(() => {
     const paypalScript = async () => {
-      const { data } = await axios.get('http://localhost:5000/api/config/paypal');
+      const { data } = await axios.get(
+        'http://localhost:5000/api/config/paypal'
+      );
       const script = document.createElement('script');
       script.type = 'text/javascript';
       script.async = true;
@@ -62,7 +64,6 @@ const CabinDetails = props => {
       } else {
         setSdkLoaded(true);
       }
-
     }
   }, [booking]);
 
@@ -70,37 +71,43 @@ const CabinDetails = props => {
   const bookingHandler = async () => {
     const newBooking = {
       checkIn: new Date('2021-6-21'), //change to calendar date picker
-      checkOut: new Date('2021-6-23') //change to calendar date picker
+      checkOut: new Date('2021-6-23'), //change to calendar date picker
     };
     try {
-      const { data } = await axios.post(`http://localhost:5000/api/cabins/${_id}`, newBooking, { 'Content-type': 'application/json' });
+      const { data } = await axios.post(
+        `http://localhost:5000/api/cabins/${_id}`,
+        newBooking,
+        { 'Content-type': 'application/json' }
+      );
       setBooking(data.booking);
-
     } catch (err) {
       setError(err.response.data.message);
     }
   };
 
   //When paypal payment is successful, update booking to paid status in database
-  const successfulPaymentHandler = async (paymentResult) => {
+  const successfulPaymentHandler = async paymentResult => {
     setLoading(true);
     try {
-      const { data } = await axios.patch(`http://localhost:5000/api/cabins/${_id}`, { paymentResult, booking }, { 'Content-type': 'application/json' });
+      const { data } = await axios.patch(
+        `http://localhost:5000/api/cabins/${_id}`,
+        { paymentResult, booking },
+        { 'Content-type': 'application/json' }
+      );
       setBooking(data.booking);
       setLoading(false);
     } catch (err) {
       setError(err.response.data.message);
       setLoading(false);
     }
-
   };
 
   return (
-    <div>
+    <div className="wrapper">
       <img src={image}></img>
 
       <Card className={classes.root}>
-        <CardContent>
+        <CardContent className="cabin-info">
           <Typography variant="h5" component="h2">
             {name}
           </Typography>
@@ -117,27 +124,46 @@ const CabinDetails = props => {
 
       {/* Calendar goes here */}
       <div className="calendar-pricing">
-        <div>
+        <div className="calendar">
+          <Typography align="center" variant="h5">
+            Select dates
+          </Typography>
           <DateRangePickerCalendar />
         </div>
 
-        <div>
-          <p>Pricing goes here</p>
+        {/* Pricing goes here */}
+        <div className="pricing">
+          <Typography align="center" variant="h5" gutterBottom>
+            Price details
+          </Typography>
+          <Pricing />
         </div>
       </div>
 
-      {/* Pricing goes here */}
-
-      <Button onClick={bookingHandler}>Book</Button>
+      <Button onClick={bookingHandler}>
+        <Typography align="center" variant="h5">
+          Book
+        </Typography>
+      </Button>
       {loading && <LoadingSpinner />}
       {error && <Message severity="error" message={error} />}
-      {booking && booking.isPaid && <Message severity="success" message="Your payment has been received! Thank you for booking with us!" />}
+      {booking && booking.isPaid && (
+        <Message
+          severity="success"
+          message="Your payment has been received! Thank you for booking with us!"
+        />
+      )}
       {/* PayPal buttons */}
       {booking && !booking.isPaid && (
         <Card className={classes.paypal}>
-          {!sdkLoaded
-            ? <LoadingSpinner />
-            : <PayPalButton amount={booking.totalPrice} onSuccess={successfulPaymentHandler} />}
+          {!sdkLoaded ? (
+            <LoadingSpinner />
+          ) : (
+            <PayPalButton
+              amount={booking.totalPrice}
+              onSuccess={successfulPaymentHandler}
+            />
+          )}
         </Card>
       )}
     </div>
