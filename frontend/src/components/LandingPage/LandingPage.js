aa4b9e63ab97dcf084d569877e3639faa206a0b7
carlosabreuh <carlos@abreuh.com>
2021-06-07 18:42:20 -0400

    Login changes

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,3 +1,4 @@
+import { Login } from '../Login/login';
 import React, { useState } from 'react';
 import MainPhoto from './MainPhoto';
 import SingleLineGrid from './SingleLineGrid';
@@ -5,9 +6,11 @@ import MultiLineGrid from './MultiLineGrid';
 import DateRangePicker from '../DateRangePicker/DatePickerCalendar';
 
 
+
 export default function LandingPage() {
   return (
     <div>
+      <Login />
       <MainPhoto />
       <MultiLineGrid />
       <SingleLineGrid />
--- frontend/src/components/LandingPage/section1Image 2.js
+++
@@ -1,8 +0,0 @@
-const section1Image = [
-  {
-    img: 'static/images/landing_page_section1_banner.jpeg',
-    title: 'Amazing views',
-  },
-];
-
-export default section1Image;
